e8bf06d05cf9ed804f2186f66ccfd93b361c12ba
wuyuewen <wuyuewen@otcaix.iscas.ac.cn>
2019-11-11 22:05:25 +0800

    test

--- collector.py
+++ collector.py
@@ -6,38 +6,78 @@ import time
 import threading
 from io import BytesIO
 from utils.utils import runCmdRaiseException
-from utils.libvirt_util import list_active_vms
+from utils.libvirt_util import list_active_vms, get_disks_spec
+from test.test_os import resource
 
-vm_resource_utilization = Gauge('vm_resource_utilization', 'The resource utilization of virtual machine', ['cpu_user_time', 'cpu_system_time', 'cpu_time', 'mem_actual', 'mem_available', 'mem_last_update'])
+vm_resource_utilization = Gauge('vm_resource_utilization', 'The resource utilization of virtual machine', \
+                                ['cpu_metrics', 'mem_metrics', 'disks_metrics', 'networks_metrics'])
 
 def collect_vm_metrics(vm):
-    resource_utilization = {'cpu_user_time': '', 'cpu_system_time': '', 'cpu_time': '', 'mem_actual': '', 'mem_available': '', 'mem_last_update': ''}
+    resource_utilization = {'cpu_metrics': {}, 'mem_metrics': {},
+                            'disks_metrics': [], 'networks_metrics': []}
     cpu_stats = runCmdRaiseException('virsh cpu-stats --total %s' % vm)
     for line in cpu_stats:
         if line.find('cpu_time') != -1:
             p1 = r'^(\s*cpu_time\s*)([\S*]+)\s*(\S*)'
             m1 = re.match(p1, line)
             if m1:
-                resource_utilization['cpu_time'] = m1.group(2)
+                resource_utilization['cpu_metrics']['cpu_time'] = m1.group(2)
         elif line.find('system_time') != -1:
             p1 = r'^(\s*system_time\s*)([\S*]+)\s*(\S*)'
             m1 = re.match(p1, line)
             if m1:
-                resource_utilization['cpu_system_time'] = m1.group(2)
+                resource_utilization['cpu_metrics']['cpu_system_time'] = m1.group(2)
         elif line.find('user_time') != -1:
             p1 = r'^(\s*user_time\s*)([\S*]+)\s*(\S*)'
             m1 = re.match(p1, line)
             if m1:
-                resource_utilization['cpu_user_time'] = m1.group(2)
+                resource_utilization['cpu_metrics']['cpu_user_time'] = m1.group(2)
     mem_stats = runCmdRaiseException('virsh dommemstat %s' % vm)
     for line in mem_stats:
         if line.find('actual') != -1:
-            resource_utilization['mem_actual'] = line.split(' ')[1].strip()
+            resource_utilization['mem_metrics']['mem_actual'] = line.split(' ')[1].strip()
         elif line.find('available') != -1:
-            resource_utilization['mem_available'] = line.split(' ')[1].strip()
+            resource_utilization['mem_metrics']['mem_available'] = line.split(' ')[1].strip()
         elif line.find('last_update') != -1:
-            resource_utilization['mem_last_update'] = line.split(' ')[1].strip()
-#     vm_resource_utilization()
+            resource_utilization['mem_metrics']['mem_last_update'] = line.split(' ')[1].strip()
+    disks_spec = get_disks_spec(vm)
+    for disk_spec in disks_spec:
+        disk_metrics = {}
+        disk_device = disk_spec[0]
+        disk_metrics['device'] = disk_device
+        stats1 = {}
+        stats2 = {}
+        blk_dev_stats1 = runCmdRaiseException('virsh domblkstat --device %s --domain %s' % (disk_device, vm))
+        for line in blk_dev_stats1:
+            if line.find('rd_req') != -1:
+                stats1['rd_req'] = int(line.split(' ')[2].strip())
+            elif line.find('rd_bytes') != -1:
+                stats1['rd_bytes'] = int(line.split(' ')[2].strip())
+            elif line.find('wr_req') != -1:
+                stats1['wr_req'] = int(line.split(' ')[2].strip())
+            elif line.find('wr_bytes') != -1:
+                stats1['wr_bytes'] = int(line.split(' ')[2].strip())
+        time.sleep(0.1)
+        blk_dev_stats2 = runCmdRaiseException('virsh domblkstat --device %s --domain %s' % (disk_device, vm))
+        for line in blk_dev_stats2:
+            if line.find('rd_req') != -1:
+                stats2['rd_req'] = int(line.split(' ')[2].strip())
+            elif line.find('rd_bytes') != -1:
+                stats2['rd_bytes'] = int(line.split(' ')[2].strip())
+            elif line.find('wr_req') != -1:
+                stats2['wr_req'] = int(line.split(' ')[2].strip())
+            elif line.find('wr_bytes') != -1:
+                stats2['wr_bytes'] = int(line.split(' ')[2].strip())
+        disk_metrics['disk_read_requests_per_secend'] = (stats2['rd_req'] - stats1['rd_req']) / 0.1 \
+        if (stats2['rd_req'] - stats1['rd_req']) >= 0 else 0
+        disk_metrics['disk_read_bytes_per_secend'] = (stats2['rd_bytes'] - stats1['rd_bytes']) / 0.1 \
+        if (stats2['rd_bytes'] - stats1['rd_bytes']) >= 0 else 0
+        disk_metrics['disk_write_requests_per_secend'] = (stats2['wr_req'] - stats1['wr_req']) / 0.1 \
+        if (stats2['wr_req'] - stats1['wr_req']) >= 0 else 0
+        disk_metrics['disk_write_bytes_per_secend'] = (stats2['wr_bytes'] - stats1['wr_bytes']) / 0.1 \
+        if (stats2['wr_bytes'] - stats1['wr_bytes']) >= 0 else 0
+        resource_utilization['disks_metrics'].append(disk_metrics)
+    #     vm_resource_utilization()
     return resource_utilization
 
 def vm_collector_threads(vm):
